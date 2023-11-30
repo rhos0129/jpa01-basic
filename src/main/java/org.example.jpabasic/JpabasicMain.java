31a7d09701bd92c6a6a4a22e6d8bfd8b1c109872
rhos0129 <rhos0129@gmail.com>
2023-11-30 18:12:00 +0900

    S07 Mapped Superclass - 매핑 정보 상속

--- src/main/java/org.example.jpabasic/JpabasicMain.java
+++ src/main/java/org.example.jpabasic/JpabasicMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class JpabasicMain {
 
@@ -17,18 +18,11 @@ public class JpabasicMain {
 
         try {
 
-            Movie movie = new Movie();
-            movie.setDirector("A");
-            movie.setActor("a");
-            movie.setName("movie");
-            movie.setPrice(10000);
-            em.persist(movie);
-
-            em.flush();
-            em.clear();
-
-            Item findMovie = em.find(Item.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
+            Member member = new Member();
+            member.setUsername("user1");
+            member.setCreateBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
+            em.persist(member);
 
             tx.commit();
 
