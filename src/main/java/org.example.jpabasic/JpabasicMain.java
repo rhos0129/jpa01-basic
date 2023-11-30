c4bf3d2cb77373e8479fa9ebd565a46496ec5fbd
rhos0129 <rhos0129@gmail.com>
2023-11-30 10:30:56 +0900

    S03 플러시

--- src/main/java/org.example.jpabasic/JpabasicMain.java
+++ src/main/java/org.example.jpabasic/JpabasicMain.java
@@ -17,43 +17,11 @@ public class JpabasicMain {
         tx.begin();
 
         try {
-//            // 비영속
-//            Member member = new Member();
-//            member.setId(100L);
-//            member.setId(101L);
-//            member.setName("HelloJPA");
-//
-//            // 영속
-//            System.out.println("=== BEFORE ===");
-//            em.persist(member); // DB에 쿼리가 바로 날라가는 것이 아니다.
-//            System.out.println("=== AFTER === ");
-
-//            // 1차캐시에서 조회 >> insert문 전에 출력된다.
-//            Member findMember = em.find(Member.class, 101L);
-//            System.out.println("findMember.id = " +findMember.getId());
-//            System.out.println("findMember.name = " +findMember.getName());
-//
-//            // 1차캐시에 없다면 DB에서 조회 >> select 문 후에 출력
-//            Member findMember2 = em.find(Member.class, 1L);
-//            System.out.println("findMember2.id = " +findMember2.getId());
-//            System.out.println("findMember2.name = " +findMember2.getName());
-
-//            // 동일성보장
-//            Member findMember3 = em.find(Member.class, 101L);
-//            Member findMember4 = em.find(Member.class, 101L);
-//            System.out.println("result = " + (findMember3 == findMember4)); //true
-
-//            // 트랜잭션을 지원하는 쓰기 지연 >> 구분선 후에 insert문 출력
-//            Member member1 = new Member(102L, "A");
-//            Member member2 = new Member(103L, "B");
-//            em.persist(member1);
-//            em.persist(member2);
-//            System.out.println("===========");
-
-            // 변경감지 >> em.persist(findMember5) 안해도 update문 출력된다.
-            Member findMember5 = em.find(Member.class, 102L);
-            findMember5.setName("Z");
-            System.out.println("===========");
+            // 플러시 직접호출 >> 구분선 전에 insert문 출력
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+            em.flush();
+            System.out.println("==============");
 
             tx.commit();
 
