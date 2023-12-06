32e257650210a9945e8f7e3fae251a0a6b9df0e3
rhos0129 <rhos0129@gmail.com>
2023-12-06 15:31:12 +0900

    S09 값 타입의 비교

--- src/main/java/org.example.jpabasic/ValueMain.java
+++ src/main/java/org.example.jpabasic/ValueMain.java
@@ -6,25 +6,14 @@ public class ValueMain {
 
     public static void main(String[] args) {
 
-        // 자바의 기본 타입은 절대 공유X
         int a = 10;
-        int b = a; // 기본 타입은 항상 값을 복사함
+        int b = 10;
+        System.out.println("a == b : " + (a == b)); // true
 
-        a = 20;
-
-        System.out.println("a = " + a); // 10
-        System.out.println("b = " + b); // 20 (절대 공유X)
-
-
-//         // Integer같은 래퍼 클래스나 String 같은 특수한 클래스는 공유는 가능하지만 값이 변경될 수 없다
-//        Integer a1 = new Integer(10);
-//        Integer b2 = a1;
-//
-//        a1 = new Integer(20); // 이거는 새로운 객체를 생성하는 것이지 값을 변경하는 것이 아니다.
-//        a1 = a1.setValue(20); // 값 변경 메소드를 지원하지 않는다.
-//
-//        System.out.println("a1 = " + a1); // 20
-//        System.out.println("b2 = " + b2); // 20 (만약 값 변경 메소드를 지원한다면 같은 참조값을 공유하게 된다. 사이드이펙트 발생.)
+        Address address1 = new Address("city", "street", "zipcode");
+        Address address2 = new Address("city", "street", "zipcode");
+        System.out.println("address1 == address2 : " + (address1 == address2)); // false 인스턴스의 참조 값을 비교
+        System.out.println("address1 equals address2 : " + (address1.equals(address2))); // true 인스턴스의 값을 비교
 
     }
 }
